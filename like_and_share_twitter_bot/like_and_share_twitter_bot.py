6ee547abe4ed668cbbd861a0acf6cd65699836ac
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2019-03-18 11:55:11 +0100

    Skipping twitter exceptions

--- like_and_share_twitter_bot/like_and_share_twitter_bot.py
+++ like_and_share_twitter_bot/like_and_share_twitter_bot.py
@@ -21,9 +21,17 @@ class LikeAndShareTwitterBot:
         with open('/tmp/liked_tweets', 'a') as myfile:
             myfile.write(selected.text)
         if random.randint(0,100) < config.GENERAL_CONFIG['chanceToFavorite']:
+          try:
             api.CreateFavorite(selected)
+          except twitter.error.TwitterError as e:
+            #Do nothing for now
+            pass
         if random.randint(0,100) < config.GENERAL_CONFIG['chanceToFollow']:
+          try:
             api.CreateFriendship(selected.user.id)
+          except twitter.error.TwitterError as e:
+            #Do nothing for now
+            pass
 
 if __name__ == '__main__':
     if random.randint(0,100) < config.GENERAL_CONFIG['chanceToAct']:
