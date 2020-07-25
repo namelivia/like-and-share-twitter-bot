11e5e7d0b4132bc5da0e78aa79e84cc1b284aab1
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-07-25 23:02:42 +0200

    Add some comments

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -22,16 +22,24 @@ class TestBot(TestCase):
         )
 
     def test_should_act(self):
+        # The random number is higher than the chance to act, do nothing
         self.random.get_percentage.return_value = 30
         self.assertFalse(self.bot.should_act())
 
+        # The random number is lower than the chance to act, do something
         self.random.get_percentage.return_value = 10
         self.assertTrue(self.bot.should_act())
 
-    def test_run(self):
+    def test_run_doing_everything(self):
         self.random.get_percentage.return_value = 10
         tweet = mock.Mock()
         self.twitter.get_tweet.return_value = tweet
         self.assertIsNone(self.bot.run())
         self.twitter.make_favorite.assert_called_once_with(tweet)
         self.twitter.follow_author.assert_called_once_with(tweet)
+
+    def test_run_doing_nothing(self):
+        self.random.get_percentage.return_value = 30
+        tweet = mock.Mock()
+        self.twitter.get_tweet.return_value = tweet
+        self.assertIsNone(self.bot.run())
